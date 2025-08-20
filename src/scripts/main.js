efa8e280d2e5817a7204b1def4911a91ab61b607
Gael Zarco <gaelxarco@icloud.com>
2025-08-19 18:31:19 -0700

    debug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,7 +24,7 @@ export function toggleTheme() {
 }
 
 export function scrollTop() {
-  requestAnimationFrame(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-  });
+  setTimeout(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, 1);
 }
